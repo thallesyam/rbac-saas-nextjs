9ef4f67a5cb5f51c99d10ecc79197bb1e3903c8e
Thalles Ian <thallesyam@gmail.com>
2025-04-26 17:37:30 -0300

    feat: create eslint, prettier and ts configs

---
+++ config/prettier/index.mjs
@@ -0,0 +1,19 @@
+/** @typedef {import('prettier').Config} PrettierConfig */
+
+/** @type { PrettierConfig } */
+const config = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  bracketSameLine: false
+}
+
+export default config
